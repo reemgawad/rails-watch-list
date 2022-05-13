c9d95a2ad1c0047c55864a07a3c0735e2ca684e0
Reem Gawad <reemabdelg@gmail.com>
2022-05-13 14:52:49 -0400

    Adjusted CSS for forms

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,8 @@
 class BookmarksController < ApplicationController
-  def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
-  end
+  # def new
+  #   @list = List.find(params[:list_id])
+  #   @bookmark = Bookmark.new
+  # end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
@@ -12,7 +12,7 @@ class BookmarksController < ApplicationController
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new
+      render 'lists/show'
     end
   end
 
